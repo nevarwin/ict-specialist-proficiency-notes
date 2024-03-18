3d77c558310a8b6d12c541d0a513fb0798237453
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-03-18 20:39:21 +0800

    created time conversion

---
+++ cpppractice/timeconversion.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int time;
+    int minutes;
+    int hour;
+
+    cout << "Enter 24hour format: ";
+    cin >> time;
+
+    cout << "time is: " << time << endl;
+
+    minutes = time % 100;
+    cout << "minutes is: " << minutes << endl;
+
+    hour = (time - minutes) / 100;
+    cout << "hour is: " << hour << endl;
+
+
+    if(hour > 23 || minutes > 59 || hour < 0 || minutes < 0){
+        cout << "Invalid time format";
+    } else {
+        cout << "12hour format is: ";
+        if (hour >= 12){
+            if (hour > 12){
+                hour -= 12;
+            }
+            cout << hour;
+            cout << (minutes < 10 ? ":0" : ":") << minutes << " PM";
+        } else if (hour == 0){
+            cout << 12;
+            cout << (minutes < 10 ? ":0" : ":") << minutes << " AM";
+        } else {
+            cout << hour;
+            cout << (minutes < 10 ? ":0" : ":") << minutes << " PM";
+        }
+    }
+    return 0;
+}
+ 
